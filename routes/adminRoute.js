ebc3d8b9544a5a5e43585b963534bcb78d7fcf43
upendrakumar Yadav <yadavupendar400@gmail.com>
2023-02-11 07:49:58 +0530

    direct approve

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -24,7 +24,7 @@ router.get("/get-all-departments", authMiddleware, async (req, res) => {
 
 router.get("/get-all-users", authMiddleware, async (req, res) => {
   try {
-    const users = await User.find({});
+    const users = await User.find({isAdmin:false});
     res.status(200).send({
       message: "User fetched successfully",
       success: true,
